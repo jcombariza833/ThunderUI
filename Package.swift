10b66747f88991b901987d26bc8d8dbab6c23c56
Juan Combariza <53232806+jcombariza833@users.noreply.github.com>
2024-07-02 15:52:43 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,19 +9,14 @@ let package = Package(
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "ThunderUI",
-      targets: ["ThunderUI", "ThunderUIKit"]),
+      targets: ["ThunderUI"]),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
-    .target(
-      name: "ThunderUI"),
     .binaryTarget(
-      name: "ThunderUIKit",
+      name: "ThunderUI",
       path: "artifacts/ThunderUIKit.xcframework"
     ),
-    .testTarget(
-      name: "ThunderUITests",
-      dependencies: ["ThunderUI"]),
   ]
 )
